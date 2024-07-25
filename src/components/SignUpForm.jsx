8f089dd2e75d21b91053f6c5bc7a24362eb482c9
LenaDurant <durant.arlena@gmail.com>
2024-07-25 13:15:41 -0400

    finished Sign Up Component tab

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,21 +4,32 @@ const SignUpForm = () => {
   let [username, setUsername] = useState("");
   let [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-
-   const handleSubmit = async(event) => {
-    event.preventDefault();  
-  }
-
+  
+  const handleSubmit = async(event) => {
+    event.preventDefault(); 
+  try {
+    const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup');
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    setError(error.message);
+}
+}
+  
 return (
   <>
    <h2>Sign Up!</h2>
+   {error && <p>There is an Error!</p>}
+
     <form onSubmit={handleSubmit}>
         <label>
           Username: <input value={username} onChange={(event) => setUsername(event.target.value)} />
         </label>
+
         <label>
-          Password: <input value= {password} onChange={(event) => setPassword(event.target.value)} />
+          Password: <input type="password" value= {password} onChange={(event) => setPassword(event.target.value)} />
         </label>
+
         <button>Submit</button>
     </form>
   </>
