0b7961ea03f1b2d550ecd31146f88c7e6b684a1b
LenaDurant <durant.arlena@gmail.com>
2024-07-25 14:04:42 -0400

    tried to finalize post request code

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 
 const SignUpForm = () => {
-  let [username, setUsername] = useState("");
-  let [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   
   const handleSubmit = async(event) => {
     event.preventDefault(); 
   try {
-    const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup');
+    const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup'),
+    {  
+    method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      })
+    }
+
     const result = await response.json();
+    setUsername("");
+    setPassword("");
+    setError("");
     console.log(result);
   } catch (error) {
     setError(error.message);
